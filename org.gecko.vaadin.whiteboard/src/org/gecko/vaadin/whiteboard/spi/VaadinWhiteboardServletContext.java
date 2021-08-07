9d6ab68be2d28f2979524dae5ba80ed9adfb755f
Stefan Bischof <stbischof@bipolis.org>
2021-08-08 00:06:59 +0200

    Simplify ServletContextHelper
    use ConstructorInjection to remove the delegate
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- org.gecko.vaadin.whiteboard/src/org/gecko/vaadin/whiteboard/spi/VaadinWhiteboardServletContext.java
+++ org.gecko.vaadin.whiteboard/src/org/gecko/vaadin/whiteboard/spi/VaadinWhiteboardServletContext.java
@@ -11,8 +11,9 @@
  */
 package org.gecko.vaadin.whiteboard.spi;
 
+import static org.gecko.vaadin.whiteboard.Constants.CM_CONTEXT;
+
 import java.net.URL;
-import java.util.Set;
 
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -20,8 +21,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.http.context.ServletContextHelper;
-
-import static org.gecko.vaadin.whiteboard.Constants.CM_CONTEXT;
 
 /**
  * Configurable {@link ServletContextHelper} that is registered under the corresponding context root from the Vaadin
@@ -38,11 +37,10 @@ import static org.gecko.vaadin.whiteboard.Constants.CM_CONTEXT;
 public class VaadinWhiteboardServletContext extends ServletContextHelper {
 	
 	private static final String META_INF_RESOURCES = "/META-INF/resources";
-	private ServletContextHelper delegatee;
 	 
     @Activate
-    private void activate(final ComponentContext ctx) {
-        delegatee = new ServletContextHelper(ctx.getUsingBundle()) {};
+    public VaadinWhiteboardServletContext(final ComponentContext ctx) {
+        super(ctx.getUsingBundle());
     }
  
     /* (non-Javadoc)
@@ -50,38 +48,13 @@ public class VaadinWhiteboardServletContext extends ServletContextHelper {
      */
     @Override
     public URL getResource(String name) {
-    	URL resourceUrl = delegatee.getResource(name);
+    	URL resourceUrl = super.getResource(name);
     	if (resourceUrl == null) {
             // this is a workaround specific for getting the correct icon.png in OSGi
             String cpPath = name.startsWith("/") ? META_INF_RESOURCES + name
                     : META_INF_RESOURCES + "/" + name;
-            resourceUrl = delegatee.getResource(cpPath);
+            resourceUrl = super.getResource(cpPath);
         }
         return resourceUrl;
-    }
- 
-    /* (non-Javadoc)
-     * @see org.osgi.service.http.context.ServletContextHelper#getMimeType(java.lang.String)
-     */
-    @Override
-    public String getMimeType(String name) {
-        return delegatee.getMimeType(name);
-    }
- 
-    /* (non-Javadoc)
-     * @see org.osgi.service.http.context.ServletContextHelper#getResourcePaths(java.lang.String)
-     */
-    @Override
-    public Set<String> getResourcePaths(String path) {
-        return delegatee.getResourcePaths(path);
-    }
- 
-    /* (non-Javadoc)
-     * @see org.osgi.service.http.context.ServletContextHelper#getRealPath(java.lang.String)
-     */
-    @Override
-    public String getRealPath(String path) {
-        return delegatee.getRealPath(path);
-    }
-
+    } 
 }

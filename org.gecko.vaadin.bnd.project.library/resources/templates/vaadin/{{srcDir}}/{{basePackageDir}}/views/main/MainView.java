69c7d33ea97d43370386f24ac8cb0fd1a349684c
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2023-02-10 13:58:37 +0100

    added project library and project template for vaadin

---
+++ org.gecko.vaadin.bnd.project.library/resources/templates/vaadin/{{srcDir}}/{{basePackageDir}}/views/main/MainView.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2012 - 2023 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+package {{basePackageName}}.views.main;
+
+import java.util.Optional;
+
+import org.gecko.vaadin.whiteboard.annotations.VaadinComponent;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+
+import {{basePackageName}}.views.helloworld.HelloWorldView;
+
+/**
+ * 
+ * @author ilenia
+ * @since Feb 7, 2023
+ */
+@Component(service=MainView.class, name="MainView", scope = ServiceScope.PROTOTYPE)
+@VaadinComponent()
+@NpmPackage(value = "lumo-css-framework", version = "3.0.11")
+public class MainView extends AppLayout {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 2078941825127700729L;
+	
+	private final Tabs menu;
+    private H1 viewTitle;
+
+    @Activate
+    public MainView() {
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        menu = createMenu();
+        addToDrawer(createDrawerContent(menu));
+    }
+
+    private com.vaadin.flow.component.Component createHeaderContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setId("header");
+        layout.getThemeList().set("dark", true);
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.add(new DrawerToggle());
+        viewTitle = new H1();
+        layout.add(viewTitle);
+        layout.add(new Avatar());
+        return layout;
+    }
+
+    private com.vaadin.flow.component.Component createDrawerContent(Tabs menu) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getThemeList().set("spacing-s", true);
+        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        VerticalLayout logoLayout = new VerticalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        logoLayout.add(new Image("images/CI_CIM.png", "DIMC logo"));
+        logoLayout.add(new H1("Vaadin App"));
+        layout.add(logoLayout, menu);
+        return layout;
+    }
+
+    private Tabs createMenu() {
+        final Tabs tabs = new Tabs();
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
+        tabs.setId("tabs");
+        tabs.add(createMenuItems());
+        return tabs;
+    }
+
+    private com.vaadin.flow.component.Component[] createMenuItems() {
+        return new Tab[]{        		
+        		createTab("Hello", HelloWorldView.class)
+        		};
+    }
+
+    private static Tab createTab(String text, Class<? extends com.vaadin.flow.component.Component> navigationTarget) {
+        final Tab tab = new Tab();
+        tab.add(new RouterLink(text, navigationTarget));
+        ComponentUtil.setData(tab, Class.class, navigationTarget);
+        return tab;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private Optional<Tab> getTabForComponent(com.vaadin.flow.component.Component component) {
+        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
+                .findFirst().map(Tab.class::cast);
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+
+}
